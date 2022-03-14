a0e81aa934ae467ac86c626d7caa4c9e0326cc93
666mandi <58552446+MatteoSeimandi@users.noreply.github.com>
2022-03-14 19:26:57 +0100

    nuovi pulsanti e nuovo layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Col, Container, Row } from "react-bootstrap";
 
 import SplashScreen from "./components/SplashScreen";
 import Title from "./components/Title";
-import { BtnPlay, BtnOptions, BtnLogin, BtnQuit, BtnMusic} from "./components/Buttons";
+import { BtnPlay, BtnOptions, BtnLogin, BtnMusic} from "./components/Buttons";
 
 import './style/App.css';
 
@@ -11,50 +11,40 @@ const App = () => {
 
 	return (
 		<>
-			<SplashScreen />
 			<Container fluid>
-				<Row className='p-3 mb-3 mt-3'>
+				<Row className='w-100 mt-5 position-absolute top-0 start-50 translate-middle-x'>
 					<Title />
 				</Row>
-				<Row className="p-3 ms-5 mb-5" xxl={3} xl={3} lg={2}>
-					<Col>
-
+				<Row className="p-3 w-75 position-absolute top-50 start-50 translate-middle">
+					<Col className="d-flex justify-content-start" xxl={4}>
+						<BtnOptions />
 					</Col>
-						<Col>
-						<Row className="my-5">
-							<BtnPlay />
-						</Row>
-						<Row className="my-5">
-							<BtnOptions />
-						</Row>
-						<Row className="my-5">
-							<Col className="p-0 me-1">
-								<BtnLogin />
-							</Col>
-							<Col className="p-0 ms-1">
-								<BtnQuit />
-							</Col>
-						</Row>
+					<Col className="d-flex justify-content-center" xxl={4}>
+						<BtnPlay />
+					</Col>
+					<Col className="d-flex justify-content-end" xxl={4}>
+						<BtnLogin />
 					</Col>
 				</Row>
-				<Row>
+				<Row className="mb-5 w-100 position-absolute bottom-0 start-50 translate-middle-x ">
 					<Col className="mt-5">
 						<p className="text-center text-white fs-1 txt-pixel">
 							The Lost Key Team
 						</p>
 					</Col>
-					<Col className="mt-5">
+					<Col className="mt-5" >
 						<p className="text-center text-white fs-1 txt-pixel">
 							Pre-Alpha 0.1.0
 						</p>
 					</Col>
 				</Row>
-				<Row className="position-absolute top-75">
+				<Row className="mb-3 position-absolute bottom-0 start-50 translate-middle">
 					<Col>
 						<BtnMusic />
 					</Col>
 				</Row>
 			</Container>
+			<SplashScreen />
 		</>
 	);
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,52 +1,50 @@
 // Bottoni
 
-import { Button } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import { BiMicrophoneOff, BiMicrophone } from "react-icons/bi";
 
 import { useAudio } from "../utils/useAudio";
 import rainSound from "../assets/sounds/Rain-sound.mp3";
+import penPlay from "../assets/img/penPlay.png";
+import penOptions from "../assets/img/penOptions.png";
+import penLogin from "../assets/img/penLogin.png";
 
 export const BtnPlay = () => {
 
 	return(
-		<Button variant="outline-dark" className="pt-2">
-			<p className="fs-1 fw-bold mb-2 txt-pixel">
-				Play
-			</p>
-		</Button>
+		<Image
+			src={penPlay}
+			height={175}
+			width={325}
+			onClick={() => alert('Il gioco inizierà da qui')}
+			fluid
+		/>
 	)
 }
 
 export const BtnOptions = () => {
 
 	return(
-		<Button variant="outline-dark" className="pt-2">
-			<p className="fs-1 fw-bold mb-2 txt-pixel">
-				Options
-			</p>
-		</Button>
+		<Image
+			src={penOptions}
+			height={175}
+			width={325}
+			onClick={() => alert('Opzioni')}
+			fluid
+		/>
 	)
 }
 
 export const BtnLogin = () => {
 
 	return(
-		<Button variant="outline-success" className="px-1 pt-2 w-100">
-			<p className="fs-1 fw-bold mb-2 txt-pixel">
-				Login
-			</p>
-		</Button>
-	)
-}
-
-export const BtnQuit = () => {
-
-	return(
-		<Button onClick={() => window.close()}  variant="outline-danger" className="pt-2 w-100">
-			<p className="fs-1 fw-bold mb-2 txt-pixel">
-				Quit
-			</p>
-		</Button>
+		<Image
+			src={penLogin}
+			height={175}
+			width={325}
+			onClick={() => alert('Login')}
+			fluid
+		/>
 	)
 }
 
@@ -55,7 +53,7 @@ export const BtnMusic = () => {
 
 	return(
 		<Button variant="secondary" onClick={toggle}>
-			{playing ? <BiMicrophone /> : <BiMicrophoneOff />}
+			{playing ? <BiMicrophone size={55} /> : <BiMicrophoneOff size={55} />}
 		</Button>
 	)
 }
